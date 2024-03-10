c0b93665acfbb165354401843e1c87a19db996fc
Odual259 <obonilla10327@ufide.ac.cr>
2024-03-10 14:31:14 -0600

    Avance 3

--- PHP_Pages/clients.php
+++ PHP_Pages/clients.php
@@ -10,20 +10,24 @@ $usuario = $_SESSION["users"];
 
 <head>
     <title>Registered Clients</title>
-    <link rel="stylesheet" type="text/css" href="styles.css">
+    <link rel="stylesheet" type="text/css" href="../Styles/styles.css">
 </head>
 
 <body>
+
 <header id="headerMenu">
-        <img id="logoDarkBackgrounds" src="https://1000logos.net/wp-content/uploads/2023/03/KPMG-logo.png" alt="KDN Project Management Tool" href="index.html">
-        <h1>KDN Project Management Tool</h1>
+    <a href="../HTML_Pages/index.html">
+        <img id="logoDarkBackgrounds" src="https://1000logos.net/wp-content/uploads/2023/03/KPMG-logo.png"
+        alt="KDN Project Management Tool">
+    </a>
+    <h1>KDN Project Management Tool</h1>
 </header>
 <nav>
-    <a href="clients.php">Clients</a>
-    <a href="calendar">Calendar</a>
-    <a href="processes">Processes</a>
-    <a href="team">Team</a>
-    <a href="news.php">News</a>
+    <a href="../PHP_Pages/clients.php">Clients</a>
+    <a href="../PHP_Pages/calendar.php">Calendar</a>
+    <a href="../PHP_Pages/processes.php">Processes</a>
+    <a href="../PHP_Pages/team.php">Team</a>
+    <a href="../PHP_Pages/news.php">News</a>
 </nav>
 <h2>Registered Clients</h2>
     <div id="container">    
