1bfe959ca0c29049b25a93344b916c8c23f8fbce
KevObando <kevinjos608@gmail.com>
2024-04-13 22:54:01 -0600

    Creacion de process.js
    Se crea el archivo process. js y se actualiza calendar.php

--- calendar.php
+++ calendar.php
@@ -6,10 +6,12 @@
 <meta charset="UTF-8">
 <meta name="viewport" content="width=device-width, initial-scale=1.0">
 <title>Calendar</title> 
+
 <script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.1.1/jquery.min.js"></script>
 <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous"></script>
-<link rel="stylesheet" type="text/css" href="Styles/calendar.css">
+<link rel="stylesheet" type="text/css" href="css/style.css">
+<script src="./js/process.js"></script>
 </head>
 
 <body>
@@ -27,7 +29,8 @@
 </header>
 
 <div id="mainContainer">
- <div id="bodyCalendar">
+
+  <div id="bodyCalendar">
     
  <h2 id="monthAndYear"><span onclick="previousMonth()">◀</span><span id="monthYearLabel"></span><span onclick="nextMonth()">▶</span></h2>
 
@@ -48,29 +51,65 @@
  <div class="filter-section">
     <h2>Filters</h2>
     <form>
-      <label for="client">Client:</label>
-      <select id="client">
-       
-      </select>
+      <div class="form-group">
+        <label for="client">Client:</label>
+        <div class="dropdown">
+          <button class="btn btn-secondary dropdown-toggle" type="button" id="dropdownMenuButton1" data-bs-toggle="dropdown" aria-expanded="false">
+            Clients
+          </button>
+          <ul class="dropdown-menu" aria-labelledby="dropdownMenuButton1">
+            <li><a class="dropdown-item" href="#">Acción</a></li>
+            <li><a class="dropdown-item" href="#">Otra acción</a></li>
+            <li><a class="dropdown-item" href="#">Algo más aqui</a></li>
+          </ul>
+        </div>
+      </div>
 
-      <label for="company">Company:</label>
-      <select id="company">
-       
-      </select>
+      <div class="form-group">
+        <label for="company">Company:</label>
+        <div class="dropdown">
+          <button class="btn btn-secondary dropdown-toggle" type="button" id="dropdownMenuButton2" data-bs-toggle="dropdown" aria-expanded="false">
+            Companies
+          </button>
+          <ul class="dropdown-menu" aria-labelledby="dropdownMenuButton2">
+            <li><a class="dropdown-item" href="#">Acción</a></li>
+            <li><a class="dropdown-item" href="#">Otra acción</a></li>
+            <li><a class="dropdown-item" href="#">Algo más aqui</a></li>
+          </ul>
+        </div>
+      </div>
 
-      <label for="process-name">Process Name:</label>
-      <select id="process-name">
-        
-      </select>
+      <div class="form-group" >
+        <label for="process-name">Process Name:</label>
+        <div class="dropdown" >
+        <button class="btn btn-secondary dropdown-toggle" type="button" id="dropdownMenuButton3" data-bs-toggle="dropdown" aria-expanded="false">
+                Procesos
+        </button>
+        <ul class="dropdown-menu" aria-labelledby="dropdownMenuButton3" id="processList">
+    
+        </ul>
+        </div>
+      </div>
 
-      <label for="country">Country:</label>
-      <select id="country">
-        
-      </select>
+      <div class="form-group">
+        <label for="country">Country:</label>
+        <div class="dropdown">
+          <button class="btn btn-secondary dropdown-toggle" type="button" id="dropdownMenuButton4" data-bs-toggle="dropdown" aria-expanded="false">
+            Countries
+          </button>
+          <ul class="dropdown-menu" aria-labelledby="dropdownMenuButton4" id="">
+            <li><a class="dropdown-item" href="#">Acción</a></li>
+            <li><a class="dropdown-item" href="#">Otra acción</a></li>
+            <li><a class="dropdown-item" href="#">Algo más aqui</a></li>
+          </ul>
+        </div>
+      </div>
     </form>
-  </div>
 </div>
+
 
 </body>
 <script src="./js/calendar.js"></script>
+
 </html>
+
---
+++ js/process.js
@@ -0,0 +1,48 @@
+$(document).ready(function () {
+    loadProcess();
+
+    function loadProcess() {
+        $.ajax({
+            url: "obtener_proceso.php",
+            method: "GET",
+            success: function (data) {
+                displayProcess(JSON.parse(data));
+            }
+        });
+    }
+
+    function displayProcess(processes) {
+        $("#processList").empty();
+        if(processes.length > 0){
+            console.log(processes);
+            processes.forEach(function (process) {
+                const listItem = `
+                <li><a class="dropdown-item" href="#"> Process: ${process.Process} Due_date: ${process.Due_date}</a></li>
+                `;
+                $("#processList").append(listItem);
+            }); 
+        }
+    }
+
+    $("#processList").on("click", ".dropdown-item", function () {
+        $.ajax({
+            url: "obtener_proceso.php",
+            method: "GET",
+            success: function (data) {
+                let dataArray = JSON.parse(data);
+                if (Array.isArray(dataArray)) {
+                    dates = [];
+                    for (let i = 0; i < dataArray.length; i++) {
+                        // Divide la cadena de fecha en partes
+                        let dateParts = dataArray[i]['Due_date'].split("-");
+                        // Crea un nuevo objeto Date a partir de las partes de la fecha
+                        // los meses comienzan desde 0, se resta 1 al mes
+                        dates.push(new Date(dateParts[0], dateParts[1] - 1, dateParts[2]));
+                    }
+                    updateCalendar();
+                }
+            }
+        });
+    });
+    
+});
