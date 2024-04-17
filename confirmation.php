97293655d300843fed3d268b9f78b1a99236cb51
EstebanTorresM <123520084+EstebanTorresM@users.noreply.github.com>
2024-04-17 16:59:07 -0600

    Create confirmation.php

---
+++ confirmation.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Confirmation</title>
+    <!-- Agrega enlaces a tus estilos CSS -->
+    <link rel="stylesheet" type="text/css" href="Styles/styles.css">
+</head>
+
+<body>
+
+<header id="headerMenu">
+    <!-- Aquí colocamos el logotipo y el título -->
+    <a href="index.html">
+        <img id="logoDarkBackgrounds" src="https://1000logos.net/wp-content/uploads/2023/03/KPMG-logo.png"
+        alt="KDN Project Management Tool">
+    </a>
+    <h1>KDN Project Management Tool</h1>
+</header>
+
+<?php include "menu.php"; ?>
+
+<h2>Confirmation</h2>
+
+<div id="container">    
+    <p>User added successfully!</p>
+    <a href="usuarios.php">Back to Users</a>
+</div>
+
+</body>
+</html>
