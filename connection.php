bc8af4ac9afd62aae05e892663cc61beae2bf1a7
KevObando <kevinjos608@gmail.com>
2024-04-23 07:41:24 -0600

    Ajuste DB

--- connection.php
+++ connection.php
@@ -1,7 +1,7 @@
 <?php
 $servername = "localhost";
 $username = "root";
-$password = "";
+$password = "root123";
 $database = "proyecto_kdn";
 
 $conn = new mysqli($servername, $username, $password, $database);
